e4f3e7eafc98ca7ac862e16d4aaee1161df78ebe
Pranta07 <palpranta@gmail.com>
2021-12-22 14:54:06 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("eMenu server is running!");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yxq3j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Database connected!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`listening to port ${port}`);
